c2fe09d836309ecc89e491684bed8cef38c72d07
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2024-09-26 22:48:28 +0100

    Add menus and buttons

--- car_racing_game.py
+++ car_racing_game.py
@@ -23,13 +23,27 @@ screen = pygame.display.set_mode((size))
 
 # load button images
 resume_img = pygame.image.load("images/button_resume.png").convert_alpha()
+options_img = pygame.image.load("images/button_options.png").convert_alpha()
+quit_img = pygame.image.load("images/button_quit.png").convert_alpha()
+video_img = pygame.image.load("images/button_video.png").convert_alpha()
+audio_img = pygame.image.load("images/button_audio.png").convert_alpha()
+keys_img = pygame.image.load("images/button_keys.png").convert_alpha()
+back_img = pygame.image.load("images/button_back.png").convert_alpha()
 
 # create button instances
 resume_button = buttons.Button(300, 125, resume_img, 1)
+options_button = buttons.Button(293, 250, options_img, 1)
+quit_button = buttons.Button(330, 375, quit_img, 1)
+video_button = buttons.Button(226, 75, video_img, 1)
+audio_button = buttons.Button(225, 200, audio_img, 1)
+keys_button = buttons.Button(246, 325, keys_img, 1)
+back_button = buttons.Button(332, 450, back_img, 1)
 
 
 # game variables
 game_paused = False
+menu_state = "main"
+game_over = False
 
 # set the font of the text
 font = pygame.font.SysFont("arialblack", 35)
@@ -50,30 +64,6 @@ def show_level():
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
-
-
-# def main_menu():
-#     while True:
-#         screen.fill((0, 0, 0))
-#         draw_text("Main Menu", font, (255, 255, 255), 200, 200)
-
-#         for event in pygame.event.get():
-#             if event.type == QUIT: # noqa
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == K_ESCAPE: # noqa
-#                 pygame.quit()
-#                 sys.exit()
-
-#         pygame.display.update()
-#         clock.tick(60)
-
-
-def game_over():
-    screen.fill((0, 0, 0))
-    over_obj = pygame.font.SysFont("comicsans", 50, True)
-    over_txt = over_obj.render("Game Over", 1, (255, 255, 255))
-    screen.blit(over_txt, (width/2, height/2))
 
 
 # draw graphics
@@ -122,16 +112,37 @@ counter = 0
 
 # game loop
 run = True
+
 while run:
     screen.fill((34, 139, 34))
 
     # check if game is paused
-    if game_paused:
-        if resume_button.draw(screen):
-            game_paused = False
+    if game_paused is True:
+        # check menu state
+        if menu_state == "main":
+            # draw pause screen buttons
+            if resume_button.draw(screen):
+                game_paused = False
+            if options_button.draw(screen):
+                menu_state = "options"
+            if quit_button.draw(screen):
+                run = False
+        if menu_state == "options":
+            # dra options buttons
+            if video_button.draw(screen):
+                print("Video settings")
+            if audio_button.draw(screen):
+                print("Audio settings")
+            if keys_button.draw(screen):
+                print("Key settings")
+            if back_button.draw(screen):
+                print("Back to main menu")
+                menu_state = "main"
+
         # display menu
-    else:
+    # else:
         # draw_text("Press SPACE to pause", font, text_col, 160, 250)
+    if game_over is not True and game_paused is not True:
         counter += 1
 
         if counter == 800:
@@ -148,14 +159,54 @@ while run:
                 car2_loc.center = right_lane, -200
         # end game
         if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 250:
-            # break
-            # game_over()
-
-            # draw_text("Game Over", font, text_col, 160, 250)
+            game_over = True
+            draw_text("Game Over", font, text_col, 160, 250)
             print("Game Over")
-            # break
 
+        # draw the road
+        pygame.draw.rect(
+            screen,
+            (50, 50, 50),
+            (width/2-road_w/2, 0, road_w, height)
+        )
+
+        # draw the center line
+        pygame.draw.rect(
+            screen,
+            (255, 240, 60),
+            (width/2-roadmark_w/2, 0, roadmark_w, height))
+
+        # draw right road mark
+        pygame.draw.rect(
+            screen,
+            (255, 255, 255),
+            (width/2+road_w/2 - roadmark_w*3, 0, roadmark_w, height))
+
+        # draw left road mark
+        pygame.draw.rect(
+            screen,
+            (255, 255, 255),
+            (width/2-road_w/2 + roadmark_w*2, 0, roadmark_w, height))
+
+        show_level()
+
+        screen.blit(car, car_loc)
+        screen.blit(car2, car2_loc)
+
+        pygame.display.update()
+    if game_over:
+        draw_text("Game Over", font, text_col, 300, 250)
+        if resume_button.draw(screen):
+            game_paused = False
+            game_over = False
+            level = 1
+            car_loc.center = left_lane, height*0.8
+            car2_loc.center = right_lane, height*0.2
+    # limits FPS to 60
+    clock.tick(60)
+    pygame.display.flip()
     # event listeners
+
     for event in pygame.event.get():
         if event.type == QUIT: # noqa
             run = False
@@ -166,41 +217,5 @@ while run:
                 car_loc = car_loc.move([+int(road_w/2), 0])
             if event.key == pygame.K_SPACE:
                 game_paused = True
-
-    # draw the road
-    pygame.draw.rect(
-        screen,
-        (50, 50, 50),
-        (width/2-road_w/2, 0, road_w, height)
-    )
-
-    # draw the center line
-    pygame.draw.rect(
-        screen,
-        (255, 240, 60),
-        (width/2-roadmark_w/2, 0, roadmark_w, height))
-
-    # draw right road mark
-    pygame.draw.rect(
-        screen,
-        (255, 255, 255),
-        (width/2+road_w/2 - roadmark_w*3, 0, roadmark_w, height))
-
-    # draw left road mark
-    pygame.draw.rect(
-        screen,
-        (255, 255, 255),
-        (width/2-road_w/2 + roadmark_w*2, 0, roadmark_w, height))
-
-    show_level()
-
-    screen.blit(car, car_loc)
-    screen.blit(car2, car2_loc)
-
-    pygame.display.update()
-
-    # limits FPS to 60
-    clock.tick(60)
-    pygame.display.flip()
 
 pygame.quit()
